85c6ffc16c46cd84c6c970dd0dbc13f71de35856
Susan Isserman <bluenote@outlook.com>
2018-11-22 21:38:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
 
   def apply_discount
     discount = (self.employee_discount/100.0)
-    item_discounted = self.add_item(price, quantity) * (1 - discount)
+    item_discounted = self.add_item * (1 - discount)
     #binding.pry
   end
 end
