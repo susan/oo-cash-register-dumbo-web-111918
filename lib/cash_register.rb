6614129c4af993d83a0a67b9acbe65b49ffd1241
Susan Isserman <bluenote@outlook.com>
2018-11-22 23:45:15 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,11 +17,11 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-     
+
     item_subtotal = price * quantity
     self.total=(self.total + item_subtotal)
 
-    
+
 end
       #binding.pry
 
@@ -38,7 +38,7 @@ end
 
   def items
     @list.flat_map { |item| [item] * quantity}
-    
+
   
 
   def void_last_transaction
