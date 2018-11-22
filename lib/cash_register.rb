ffdcffcc77d974014588120022bf262cb01732a6
Susan Isserman <bluenote@outlook.com>
2018-11-22 23:19:02 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 
 require 'pry'
 class CashRegister
-  attr_accessor :total, :employee_discount, :title, :price, :quantity, :last_transaction
+  attr_accessor :total, :employee_discount, :title, :price, :quantity, :list, :last_transaction
 
   def initialize(total = 0, *employee_discount)
     @total = total
