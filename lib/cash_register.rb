95fb04f3c8cac41b8170e8ea74705b337377a076
Susan Isserman <bluenote@outlook.com>
2018-11-22 20:24:08 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,10 +3,14 @@ require 'pry'
 class CashRegister
   attr_accessor :total, :employee_discount
 
-  def initialize(total = 0, *employee_discount)
+  def initialize(total = 0, *employee_discount = 20)
     @total = total
     @employee_discount = employee_discount
   end
+  def discount
+    @employee_discount
+  end  
 end
-kroger = CashRegister.new
+cash_register = CashRegister.new
+cash_register_with_discount = cash_register.discount
 #binding.pry
