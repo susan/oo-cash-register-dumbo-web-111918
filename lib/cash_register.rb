32c146114a85bc48497930bcc8d894e010e859b9
Susan Isserman <bluenote@outlook.com>
2018-11-22 21:54:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,10 +16,8 @@ class CashRegister
   def add_item(title, price, *quantity)
       if quantity == nil
         quantity = 1
-      else
-        quantity
       end
-        item_subtotal = price * quantity.to_f
+        item_subtotal = price * quantity
         self.total=(self.total + item_subtotal)
       #binding.pry
       end
