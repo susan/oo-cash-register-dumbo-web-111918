131dff06aa6c2d16bd5e8851a4ab521d0ae8046e
Susan Isserman <bluenote@outlook.com>
2018-11-22 20:31:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,6 +6,7 @@ class CashRegister
   def initialize(total = 0, *employee_discount)
     @total = total
     @employee_discount = employee_discount
+    @employee_discount = 20
   end
 
   # def discount
