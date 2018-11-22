69af66cdd84df3158ba7e3b2ff508000a538f09f
Susan Isserman <bluenote@outlook.com>
2018-11-22 23:48:37 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-   list.flatten << [title] * quantity
+   list = list.flatten << [title] * quantity
     item_subtotal = price * quantity
     self.total=(self.total + item_subtotal)
      end
