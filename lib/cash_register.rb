e1d99186b97f1c6228a52ea79ca717d1b39615c8
Susan Isserman <bluenote@outlook.com>
2018-11-22 23:07:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,9 +21,12 @@ end
       #binding.pry
 
   def apply_discount
-    discount = (self.employee_discount/100.0)
-    item_discounted = self.add_item(title, price) * (1 - discount)
-    binding.pry
+    if !employee_discount
+      puts "theere is no discount"
+    else  
+      discount = (self.employee_discount/100.0)
+      item_discounted = self.add_item(title, price) * (1 - discount)
+    end
   end
 
   def void_last_transaction
